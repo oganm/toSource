a5d95c1b4dede821df45354c466598c4039dc916
OganM <ogan.mancarcii@gmail.com>
2014-09-18 15:50:11 -0700

    GSEdown function added to GEO.r

--- GEO.r
+++ GEO.r
@@ -1,19 +1,39 @@
 # replacemt to gsmDown. Didn't remove it in case I'm using it somewhere. This
 # will include all GEO related functions
+eval( expr = parse( text = getURL(
+    "https://raw.githubusercontent.com/oganm/toSource/master/ogbox.r",
+    ssl.verifypeer=FALSE) ))
+
 
 GSMfind = function(GSE, regex=''){
-    require(RCurl)
+    # finds GSMs that match to a regular expression from a GSE (description not GSM ID)
+    library(RCurl)
     page = getURL(paste0('www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=', GSE))
-    gsms = regmatches(page,gregexpr(paste0('GSM[0-9]*?(?=<.*\n.*?',regex,'</td)'),page,perl=T))
+    gsms = regmatches(page,gregexpr(paste0('GSM[0-9]*?(?=<.*\n.*?',regex,'</td)'),page,perl=T))[[1]]
     return(gsms)
 }
 
 gsmDown = function(gsm,outfile){
+    # downloads a given GSM
+    library(RCurl)
     page = getURL(paste0('http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=',gsm))
-
     download.file(
         gsubMult(c('%5F','%2E','%2D'),
                  c('_'  , '.', '-'),
                  regmatches(page,gregexpr('ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM.*?gz',page,perl = T))[[1]]),
         outfile)
+}
+
+
+GSEdown = function(GSE,regex ='',outDir,progBar = T, extension = '.cel'){
+    # downloads GSMs matching a regular expression from a GSE (description not GSM ID)
+    library(RCurl)
+    gsms = GSMfind(GSE, regex)
+    if (progBar == T){
+        pb = txtProgressBar(min = 1, max = length(gsms),initial = 1)
+    }
+    for (i in 1:length(gsms)){
+        if (progBar ==T){setTxtProgressBar(pb,i)}
+        gsmDown(gsms[i],paste0(outDir,'/', gsms[i],extension))
+    }
 }
