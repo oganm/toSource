fa52639390d1061849e9be6032d7ac3c86c229d1
OganM <ogan.mancarcii@gmail.com>
2014-11-26 11:15:20 -0800

    gsmDown removed, it's already inside GEO.r

--- gsmDown.r
+++
@@ -1,9 +0,0 @@
-gsmDown = function(gsm,outfile){
-    page = getURL(paste0('http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=',gsm))
-    
-    download.file(
-        gsubMult(c('%5F','%2E','%2D'),
-                 c('_'  , '.', '-'),
-                 regmatches(page,gregexpr('ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM.*?gz',page,perl = T))[[1]]),
-        outfile)
-}
