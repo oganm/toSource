ead62bf01343b0f1dcf26363083df08cc160fb1d
OganM <ogan.mancarcii@gmail.com>
2015-01-22 14:23:26 -0800

    obgox: list by Grothendieck added

--- ogbox.R
+++ ogbox.R
@@ -1,3 +1,18 @@
+# allows directly assigning list outputs to variables
+# http://stackoverflow.com/questions/1826519/function-returning-more-than-one-value
+list <- structure(NA,class="result")
+"[<-.result" <- function(x,...,value) {
+    args <- as.list(match.call())
+    args <- args[-c(1:2,length(args))]
+    length(value) <- length(args)
+    for(i in seq(along=args)) {
+        a <- args[[i]]
+        if(!missing(a)) eval.parent(substitute(a <- v,list(a=a,v=value[[i]])))
+    }
+    x
+}
+
+
 # just a lazy way to get the last value
 ans = function(){
     .Last.value
