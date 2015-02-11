5ac12302fd48b76f482b4b52a86470b667d0963e
Ogan Mancarci <ogan.mancarcii@gmail.com>
2015-02-11 13:57:57 -0800

    ogbox: alright it is fixed now

--- ogbox.R
+++ ogbox.R
@@ -261,30 +261,26 @@ listStrW = function(daArray){
 
 # turn every member of daList to a color from the palette
 toColor = function(daList, palette = NA){
-
     daList = as.factor(daList)
     uniq = unique(daList)
     if (is.na(palette[1])){
         palette = rainbow(length(uniq))
-        names(palette) = uniq
     }
+    names(palette) = uniq
     cols = vector (length = length(daList))
-
     #to match palette names to uniq names so that custom naming is possible
     if (!is.null(names(palette))){
         palette = trimNAs(palette[match(uniq,names(palette))])
         names(palette) = uniq
     }
-
-
+    
     for (i in 1:length(uniq)){
         cols[daList == uniq[i]]= palette[i]
     }
-
     out = list()
     out$cols = cols
     out$palette = palette
-
+    
     return(out)
 }
 
